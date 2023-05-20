90984cf2e5839a03f29887ca41b4b7551bf59858
delsint123 <delsint123@gmail.com>
2023-05-20 19:44:17 -0400

    Generate array of 10 random numbers for dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import Die from "./components/Die"
 
 export default function App() {
 
+    function allNewDice() {
+        const randomDice = []
+
+        for(let i = 0; i < 10; i++) {
+            randomDice.push(Math.ceil(Math.random() * 6))
+        }
+
+        return randomDice
+    }
+
+    console.log(allNewDice())
+
     const dies = []
 
     for(let i = 0; i < 10; i++) {
