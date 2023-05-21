abe717264af90940ce01f3b92d95aaa26fa741d5
delsint123 <delsint123@gmail.com>
2023-05-20 23:38:58 -0400

    Add styling for held dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Die(props) {
     return (
         <div>
-            <p className="die-face">{props.value}</p>
+            <p className={props.isHeld ? "die-face-held" : "die-face"}>{props.value}</p>
         </div>
     )
 }
