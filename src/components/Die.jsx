9612502e65fa9f1d865bf25eed4b1f1c6f8343a2
delsint123 <delsint123@gmail.com>
2023-05-21 02:33:25 -0400

    Create and style dots for die face

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { nanoid } from "nanoid";
 
 export default function Die(props) {
+
+    const dots = [];
+
+    for(let i = 0; i < props.value; i++) {
+        dots.push(<span key={nanoid()} className="dot"></span>)
+    }
+
     return (
         <div>
-            <p 
-                className={props.isHeld ? "die-face-held" : "die-face"}
+            <div 
+                className={props.isHeld ? `die-face-held face-${props.value}` : `die-face face-${props.value}`}
                 onClick={() => props.holdDice(props.id)}
-            >{props.value}</p>
+            >{dots}</div>
         </div>
     )
 }
